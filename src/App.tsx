b9d709de568c28ee348d356998bfd13d60e17ba1
Adediran babatomiwa <babatomm@gmail.com>
2025-05-08 12:21:21 +0100

    update

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,7 @@ export default function App() {
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-50">
-      <h1 className="text-3xl font-bold mb-6">🌐 IncoTest Wallet App</h1>
-
+      
       <ConnectButton />
 
       {isConnected && (
